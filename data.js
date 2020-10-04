34008bedd1c971fb4f2ba815f10f81bdf9857745
Dachachan <614259036@webmail.npru.ac.th>
2020-10-04 13:23:10 +0700

    v1

---
+++ data.js
@@ -0,0 +1,13 @@
+const data = {
+    "name":"Dachachan Buasang",
+    "work":"Studen",
+    "aboutme":{
+        "FullName":"Dachachan Buasang",
+        "Phone":"(+66)0980021357",
+        "Email":"614259036@webmail.npru.ac.th",
+        "Website":"https://www.facebook.com/profile.php?id=100023722341579",
+        "Address":"7/1 1 srakatiem Nakornpathom"
+    }
+
+};
+module.exports = data;
